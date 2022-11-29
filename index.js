220263e6dbd742f96f96ad30adaee217db1ac317
DDatj <jsmsj04@gmail.com>
2022-11-29 16:57:44 +0700

    update routes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', routes);
+app.use('/api/v1', routes);
 
 app.listen(process.env.PORT || 3000, () => {
    console.log(`Server running on port ${process.env.PORT || 3000}`);
